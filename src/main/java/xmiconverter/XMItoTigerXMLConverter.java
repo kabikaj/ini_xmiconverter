22b60a6554efb7a43aebbb04b14449cc90c7e858
alrazi <fovi002@uni-hamburg.de>
2016-07-06 17:17:38 +0200

    Add changes

--- src/main/java/xmiconverter/XMItoTigerXMLConverter.java
+++ src/main/java/xmiconverter/XMItoTigerXMLConverter.java
@@ -5,11 +5,9 @@ import org.apache.uima.collection.CollectionReaderDescription;
 import org.apache.uima.fit.factory.AnalysisEngineFactory;
 import org.apache.uima.fit.factory.CollectionReaderFactory;
 import org.apache.uima.fit.pipeline.SimplePipeline;
-
-//import de.tudarmstadt.ukp.dkpro.core.io.tcf.TcfWriter;
 import de.tudarmstadt.ukp.dkpro.core.io.tiger.TigerXmlWriter;
 import de.tudarmstadt.ukp.dkpro.core.io.xmi.XmiReader;
-//import de.tudarmstadt.ukp.dkpro.core.io.xmi.XmiWriter;
+import de.tudarmstadt.ukp.dkpro.core.testing.dumper.CasDumpWriter;
 
 
 public class XMItoTigerXMLConverter {
@@ -20,30 +18,13 @@ public class XMItoTigerXMLConverter {
 				XmiReader.PARAM_SOURCE_LOCATION, "target/output/arabic_example.xmi",
 				XmiReader.PARAM_LANGUAGE, "en");
 		
-		//AnalysisEngineDescription writer = AnalysisEngineFactory.createEngineDescription(RelAnnisWriter.class,
-		//		RelAnnisWriter.PARAM_PATH, "target/tiger_output");
-		
-	
-		//FIXME doesn't work
-		//AnalysisEngineDescription writer = AnalysisEngineFactory.createEngineDescription(TcfWriter.class,
-		//		TcfWriter.PARAM_TARGET_LOCATION, "target/output");
-
-		//FIXME doesn't work
-		//AnalysisEngineDescription writer = AnalysisEngineFactory.createEngineDescription(InlineXmlWriter.class,
-		//		InlineXmlWriter.PARAM_TARGET_LOCATION, "target/output");
-		
-		//FIXME doesn't work
-		//AnalysisEngineDescription writer = AnalysisEngineFactory.createEngineDescription(XmiWriter.class,
-		//		XmiWriter.PARAM_TARGET_LOCATION, "target/output");
-
-		//FIXME doesn't convert custom layers
+		// tigerxml does not support custom annotation layers
 		AnalysisEngineDescription writer = AnalysisEngineFactory.createEngineDescription(TigerXmlWriter.class,
 				TigerXmlWriter.PARAM_TARGET_LOCATION, "target/output");
 		
-		//AnalysisEngineDescription dumper = AnalysisEngineFactory.createEngineDescription(
-		//		CasDumpWriter.class);
+		AnalysisEngineDescription dumper = AnalysisEngineFactory.createEngineDescription(
+				CasDumpWriter.class);
 		
-		SimplePipeline.runPipeline(reader, writer);
-	}
+		SimplePipeline.runPipeline(reader, writer, dumper);	}
 	
 }
